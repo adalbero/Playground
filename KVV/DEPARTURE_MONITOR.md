3fcb5b694bfa0a76f5af3f3bcadbb9b4d1b260c1
Adalbero F. Guimaraes <adalbero@gmail.com>
2025-02-16 20:21:12 +0100

    [KVV] v1

---
+++ KVV/DEPARTURE_MONITOR.md
@@ -0,0 +1,55 @@
+# KVV API
+
+## Endpoints
+
+https://www.kvv.de/tunnelEfaDirect.php?action=XSLT_DM_REQUEST
+https://projekte.kvv-efa.de/json
+https://projekte.kvv-efa.de/sl3/XSLT_DM_REQUEST
+https://projekte.kvv-efa.de/sl3-alone/XSLT_DM_REQUEST
+
+## outputFormat
+JSON
+XML
+
+## language
+en
+de
+fr
+
+## coordOutputFormat
+WGS84[dd.ddddd]
+
+## type_dm
+stop
+stopID
+
+## name_dm
+
+7003205 | Hagsfeld Schäferstraße
+7003102 | Hagsfeld Hagsfeld Bahnhof
+7000231 | Hagsfeld Hagsfeld Süd
+7001001 | KIT/Campus South (U)
+
+accepts:
+name (KIT/Campus South (U))
+stateless ID (7001001) 
+GID (de:08212:1001)
+
+## itdLPxx_template: 
+tableResults
+tooltip
+dmresults
+
+## CoordSystem
+WGS84
+
+## mode
+direct
+
+## limit
+40
+100
+
+# References:
+https://verkehrsforum-karlsruhe.de/threads/was-ist-mit-der-kvv-live-api-passiert.280/
+https://github.com/schildbach/public-transport-enabler
---
+++ KVV/index.js
@@ -0,0 +1,117 @@
+const axios = require('axios');
+
+const STOP = {
+    HAGSFELD_BAHNHOF: '7003102',
+    HAGSFELD_SUD: '7000231',
+}
+
+const LINE = {
+    S2: {
+        symbol: 'S2',
+        stops: [
+            {
+                stop: STOP.HAGSFELD_BAHNHOF,
+                toCenter: '3',
+                fromCenter: '4',
+            },
+            {
+                stop: STOP.HAGSFELD_SUD,
+                toCenter: '1',
+                fromCenter: '2',
+            }
+        ]
+    }
+}
+
+const DIRECTION = {
+    TO_CENTER: 'toCenter',
+    FROM_CENTER: 'fromCenter',
+}
+
+async function fetchData(params) {
+    try {
+        const response = await axios.get('https://projekte.kvv-efa.de/sl3/XSLT_DM_REQUEST', {
+            params: {
+                outputFormat: 'JSON',
+                language: 'en',
+                stateless: '1',
+                coordOutputFormat: 'WGS84[DD.ddddd]',
+                type_dm: 'stop',
+                itdTime: '0000',
+                useRealtime: '1',
+                mode: 'direct',
+                limit: 1000,
+                ...params
+            },
+            headers: { Accept: '*/*', 'User-Agent': 'Thunder Client (https://www.thunderclient.com)' }
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return {};
+    }
+}
+
+
+
+async function fetchDayTimeTable(date, stop, line, direction) {
+    const strDate = formatDate(date);
+
+    const params = {
+        name_dm: stop,
+        itdDate: strDate,
+    };
+
+    const json = await fetchData(params);
+
+    const symbol = line.symbol;
+    const platform = line.stops.find(item => item.stop === stop)[direction];
+    json.departureList = json.departureList.filter(dep => dep.platform === platform && dep.servingLine.symbol === symbol);
+
+    const item = json.departureList[0];
+    return {
+        stop: item.stopName,
+        line: line.symbol,
+        platform: platform,
+        destination: direction,
+        weekday: date.getDay() == 6 ? 'Saturday' : date.getDay() == 0 ? 'Sunday' : 'Weekday',
+        timetable: json.departureList.map(dep => formatTime(dep.dateTime))
+    }
+}
+
+async function getTimeTable(stop, line, direction) {
+    const result = [];
+    const today = new Date();
+    for (let i = 0; i < 3; i++) {
+        const date = addDays(today, i - 1 - today.getDay());
+        const json = await fetchDayTimeTable(date, stop, line, direction);
+        result.push(json);
+    }
+
+    return result;
+}
+
+function pad(num) {
+    return String(num).padStart(2, '0');
+}
+
+function formatDate(date) {
+    return `${date.getFullYear()}${pad(date.getMonth() + 1)}${pad(date.getDate())}`;
+}
+
+function formatTime(dateTime) {
+    return `${pad(dateTime.hour)}:${pad(dateTime.minute)}`;
+}
+
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+(async () => {
+    console.log('BEGIN');
+    const result = await getTimeTable(STOP.HAGSFELD_SUD, LINE.S2, DIRECTION.FROM_CENTER);
+    console.log(result);
+})();
+
